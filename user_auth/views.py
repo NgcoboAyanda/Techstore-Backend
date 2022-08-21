5784f5a392a6e999e0d70c8e9bfa9d7589b5b321
Ayanda Ngcobo <ayanda13@protonmail.com>
2022-08-21 16:54:41 +0200

    edited urls

--- user_auth/views.py
+++ user_auth/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
 
 # Create your views here.
+class AuthView(View):
+    def get(self, request):
+        return HttpResponse('<h1> This is the auth page </h1>')
