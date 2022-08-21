7e0496353d3c32de0d7a591537b6f40f15bed9ab
Ayanda Ngcobo <ayanda13@protonmail.com>
2022-08-21 19:14:30 +0200

    fixed signup

--- user_auth/views.py
+++ user_auth/views.py
@@ -1,8 +1,43 @@
 from django.shortcuts import render
+from django.db.utils import IntegrityError
 from django.http import HttpResponse
-from django.views import View
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.exceptions import APIException
+
+from user_auth.models import MyUser
+
+# 409 Exception
+class EmailAlreadyExists(APIException):
+    status_code = 409
+    default_detail = 'Error! A user with that email address already exists.'
+    default_code = 'Email Already Exists.'
 
 # Create your views here.
-class AuthView(View):
-    def get(self, request):
-        return HttpResponse('<h1> This is the auth page </h1>')
+class SignupView(APIView):
+    """
+    A simple View for signing up.
+    *Only takes POST request*
+    *Returns status code 200 on success*
+    """
+    def createUser(self, request_object):
+        data = request_object.data
+        email = data['email']
+        first_name = data['first_name']
+        last_name = data['last_name']
+        dob = data['date_of_birth']
+        
+        try:
+            #creating the new user
+            MyUser.objects.create(email=email, first_name=first_name, last_name=last_name, date_of_birth=dob)
+            return {'Message':'User successfully registered. You can now log in.'}
+        
+        except(IntegrityError):
+        #An integrity error will be raised if the email address is associated with another account.
+            raise EmailAlreadyExists
+
+
+    def post(self, request):
+        resp = self.createUser(request_object=request)
+        return Response(resp)
+
