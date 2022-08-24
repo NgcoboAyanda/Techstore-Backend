527a2e4e656ee7296dfc5707a1cd40fbf9ea3099
Ayanda Ngcobo <ayanda13@protonmail.com>
2022-08-24 17:42:20 +0200

    added validate function

--- user_auth/exceptions.py
+++ user_auth/exceptions.py
@@ -10,12 +10,26 @@ class EmailAlreadyExists(APIException):
     default_detail = 'Error! A user with that email address already exists.'
     default_code = 'Email Already Exists.'
 
-# DATE-OF-BIRTH ERRO
+class InvalidEmail(ValidationError):
+    default_detail = 'Invalid email address!'
+    default_code = 'Invalid Email!'
+
+
+
+# DATE-OF-BIRTH EXCEPTIONS
 class InvalidDateOfBirth(ValidationError):
-    default_detail = 'Invalid date_of_birth!'
+    default_detail = 'Invalid Date Of Birth!'
     default_code = 'Invalid dob.'
+
+
 
 # PASSWORD EXCEPTIONS
 class PasswordTooShort(ValidationError):
-    default_detail = 'Password is too short!'
+    default_detail = 'Password has less than 6 characters. Password is too short!'
     default_code = 'Short Password.'
+
+
+# OTHER EXCEPTIONS
+class InvalidInformation(ValidationError):
+    default_detail = 'Required fields have not been submitted!'
+    default_code = 'Required Fields Not Submitted.'
