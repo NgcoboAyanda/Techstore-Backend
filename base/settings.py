8f1a715703ab4c3a7d786b65721cc2102aef457b
NgcoboAyanda <ayanda13@protonmail.com>
2022-12-30 14:48:12 +0200

    added first migrations

--- base/settings.py
+++ base/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # False if not in os.environ because of casting above
-DEBUG = False
+DEBUG = True
 
 APPEND_SLASH=False
 ALLOWED_HOSTS = ["*"]
