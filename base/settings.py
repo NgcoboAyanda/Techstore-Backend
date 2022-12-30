21f515be66305576c5a494ea28e6aca131c8fded
NgcoboAyanda <ayanda13@protonmail.com>
2022-12-30 15:04:21 +0200

    turned DEBUG to False

--- base/settings.py
+++ base/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # False if not in os.environ because of casting above
-DEBUG = False
+DEBUG = env('DEBUG')
 
 APPEND_SLASH=False
 
