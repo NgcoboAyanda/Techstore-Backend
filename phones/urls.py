41e422525b82aa6731c2c5553da6ecf91723b631
NgcoboAyanda <ayanda13@protonmail.com>
2023-01-03 21:20:57 +0200

    fix phones url

--- phones/urls.py
+++ phones/urls.py
@@ -6,5 +6,5 @@ from phones.serializers import PhoneSerializer
 
 urlpatterns = [
     path('phones/', ListAPIView.as_view(queryset=Phone.objects.all(), serializer_class=PhoneSerializer), name='list-phones'),
-    path('phones/<int:pk>', RetrieveAPIView.as_view(queryset=Phone.objects.all(), serializer_class=PhoneSerializer), name='get-single-phone')
+    path('phones/<int:pk>/', RetrieveAPIView.as_view(queryset=Phone.objects.all(), serializer_class=PhoneSerializer), name='get-single-phone')
 ]
